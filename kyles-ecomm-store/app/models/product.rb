1cb7c3cfd1abf24fe08bc096ee5ba9d5c7392aa5
brett-anderson <brettanderson16@gmail.com>
2013-11-13 10:37:46 -0600

    Added an empty store controller

--- kyles-ecomm-store/app/models/product.rb
+++ kyles-ecomm-store/app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: true
 end
--- kyles-ecomm-store/config/routes.rb
+++ kyles-ecomm-store/config/routes.rb
@@ -1,4 +1,6 @@
 KylesEcommStore::Application.routes.draw do
+  get "store/index"
+
   resources :products
 
 
