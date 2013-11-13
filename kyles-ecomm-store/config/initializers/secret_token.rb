863c9fb056b0e16d5fbb74dace0e593c9d892fcd
brett-anderson <brettanderson16@gmail.com>
2013-11-13 10:13:26 -0600

    added empty rails project

---
+++ kyles-ecomm-store/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KylesEcommStore::Application.config.secret_token = 'e2622df755da1073c862ce1fb0f6ce4656d524030aaf2653ac3858627e1df737876c268217ef7a088a71365a411e50d2981aa7fd255701349e691f754ad1d265'
